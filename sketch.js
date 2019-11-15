6a3ed07ba4312fa2bde646b4d4f6e7dc1e3da6c5
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-11-15 14:10:47 -0600

    Installed p5js files

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// Major Project
+// Eric James
+// Nov. Fri. 15th 2019
+//
+// Extra for Experts:
+// - 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
