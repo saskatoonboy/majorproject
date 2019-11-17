4377615a24ba1781cf1c61137fd2155f8c6b54a0
BuildTools <unconfigured@null.spigotmc.org>
2019-11-17 14:28:58 -0600

    added food class

---
+++ food.js
@@ -0,0 +1,22 @@
+class Pellet {
+    constructor(x, y, energy) {
+        if (energy === undefined) {
+            this.energy = 100;
+        } else {
+            this.energy = energy;
+        }
+        this.size = this.energy/100;
+        this.pos = createVector(x, y);
+    }
+
+    display() {
+        translate(this.pos.x, this.pos.y);
+        fill(41, 140, 72);
+        circle(0, 0, this.size);
+        translate(-this.pos.x, -this.pos.y);
+    }
+
+    update() {
+        this.size = this.energy/100;
+    }
+}
