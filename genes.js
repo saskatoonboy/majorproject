fa1d1a28b82f81d39b9e06880d6c1c05713c7ca7
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-11-18 13:47:32 -0600

    move brain gene classes into the genes file

--- genes.js
+++ genes.js
@@ -16,3 +16,63 @@ let gene = {
         };
     }
 };
+
+// Brain Genes
+
+class ConnectionGenenome {
+
+    // constructor
+    constructor(connectionGenes) {
+
+        this.genes = connectionGenes;
+
+    }
+
+    // get a list of nodes from the connection genenome
+    unpackNodes() {
+        let nodesList = [];
+        let localNodes = [];
+        for (connectionGene of this.genes) {
+            if (nodesList.indexOf(connectionGene.from) === -1) {
+                nodesList.push(connectionGene.from);
+            }
+            if (nodesList.indexOf(connectionGene.to) === -1) {
+                nodesList.push(connectionGene.to);
+            }
+        }
+
+        nodesList.sort((a, b) => a - b);
+
+        for (nodeId in nodesList) {
+            localNodes = nodes[nodeId];
+        }
+
+        return localNodes;
+
+    }
+
+    // get a list of connections from the connection genenome
+    unpackConnections() {
+        let localConnections = [];
+        for (connectionGene of this.genes) {
+            localConnections.push(connectionGene.getConnection());
+        }
+        return localConnections;
+    }
+}
+
+class ConnectionGene {
+    
+    // constructior
+    constructor(identification, from, to, enabled, weight) {
+        this.identificationNumber = identification;
+        this.from = from;
+        this.to = to;
+        this.enabled = enabled;
+        this.weight = weight;
+    }
+
+    getConnection() {
+        return connections[this.identificationNumber];
+    }
+}
