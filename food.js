c257fc0f46cfef52fc745f0014f08a262b6127ca
BuildTools <unconfigured@null.spigotmc.org>
2019-11-25 08:16:20 -0600

    testing collison and consumtion of food

--- food.js
+++ food.js
@@ -1,11 +1,13 @@
 class Pellet {
     constructor(x, y, energy) {
+        this.maxEnergy = 500;
         if (energy === undefined) {
-            this.energy = 100;
+            this.energy = this.maxEnergy;
         } else {
             this.energy = energy;
+            this.maxEnergy = energy;
         }
-        this.size = this.energy/100;
+        this.size = this.energy/this.maxEnergy;
         this.pos = createVector(x, y);
     }
 
@@ -17,6 +19,24 @@ class Pellet {
     }
 
     update() {
-        this.size = this.energy/100;
+        this.size = this.energy/this.maxEnergy;
+    }
+
+    collison(creature) {
+        return creature.distance(this) <= creature.sizeRatio*25+this.size*5;
+    }
+
+    remove() {
+        foods.splice(foods.indexOf(this), 1);
+    }
+
+    consume(stomachSpace) {
+        if (stomachSpace >= this.energy) {
+            this.remove();
+            return this.energy;
+        } else {
+            this.energy -= stomachSpace;
+            return stomachSpace;
+        }
     }
 }
